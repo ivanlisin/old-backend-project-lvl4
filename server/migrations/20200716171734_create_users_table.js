7a8a08e014e4f1cda2478a45850ae5e071fb913b
Ivan Lisin <ivanlisin.tech@gmail.com>
2022-01-04 18:36:32 +0700

    add fields for user

--- server/migrations/20200716171734_create_users_table.js
+++ server/migrations/20200716171734_create_users_table.js
@@ -3,6 +3,8 @@
 exports.up = (knex) => (
   knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
+    table.string('first_name');
+    table.string('last_name');
     table.string('email');
     table.string('password_digest');
     table.timestamp('created_at').defaultTo(knex.fn.now());
