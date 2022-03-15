119e63ab15464a6b655c657d19917b637f0fcd7d
Ivan Lisin <ivanlisin.tech@gmail.com>
2022-03-15 17:12:04 +0700

    update(app layout): hide statuses link for not auth users

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -1,8 +1,18 @@
+import i18next from 'i18next';
+
 export default (app) => {
   app
-    .get('/statuses', { name: 'statuses' }, async (req, reply) => {
+    .get('/statuses', { name: 'statuses' }, app.fp.authenticate('check', async (req, reply, err, user) => {
+      if (err) {
+        return app.httpErrors.internalServerError(err);
+      }
+      if (!user) {
+        req.flash('info', i18next.t('flash.authError'));
+        reply.redirect(app.reverse('root'));
+        return reply;
+      }
       const statuses = await app.objection.models.taskStatus.query();
       reply.render('statuses/index', { statuses });
       return reply;
-    });
+    }));
 };
