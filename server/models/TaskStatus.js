820795a4a053dca5e9fabb0af408224392858cf3
Ivan Lisin <ivanlisin.tech@gmail.com>
2022-04-11 15:13:34 +0700

    fix task status model

--- server/models/TaskStatus.js
+++ server/models/TaskStatus.js
@@ -17,7 +17,7 @@ export default class TaskStatus extends unique(Model) {
         'name',
       ],
       properties: {
-        // id: { type: 'integer' },
+        id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
       },
     };
